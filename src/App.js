692a8c81c61e3d2f29c202889343a4fc75018c98
2014031930 <2014031930@hanyang.ac.kr>
2021-01-26 14:25:42 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import logo from './logo.svg';
+// import React, { Component } from 'react';
+import Customer from './components/Customer';
 import './App.css';
+
+const customers = [
+  {
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : '홍길동',
+  'birthday' : '990419',
+  'gender' : '남성',
+  'job' : '대학생'  
+ },
+ {
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '김신',
+  'birthday' : '970709',
+  'gender' : '남성',
+  'job' : '프로그래머'  
+ },
+ {
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '김영희',
+  'birthday' : '920719',
+  'gender' : '여자',
+  'job' : '직장인'  
+ }
+]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
+      <div>
+        {customers.map(c => { return ( <Customer key = {c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+      </div>
   );
 }
 
