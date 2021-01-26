d399b24d9903ca4c91c85d9654566af3bd8ac149
2014031930 <2014031930@hanyang.ac.kr>
2021-01-26 19:17:00 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -16,38 +16,31 @@ const styles = theme => ({
     overflowX: "auto"
   },
   table: {
-    minWidth: 720
+    minWidth: 1080
   }
 })
 
-const customers = [
-  {
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/any',
-  'name' : '홍길동',
-  'birthday' : '990419',
-  'gender' : '남성',
-  'job' : '대학생'  
- },
- {
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '김신',
-  'birthday' : '970709',
-  'gender' : '남성',
-  'job' : '프로그래머'  
- },
- {
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '김영희',
-  'birthday' : '920719',
-  'gender' : '여자',
-  'job' : '직장인'  
- }
-]
+
 
 class App extends Component {
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));  
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,7 +57,9 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-              {customers.map(c => { return ( <Customer key = {c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);})}
+              {this.state.customers ? this.state.customers.map(c => {
+                return ( <Customer key = {c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+              }) : ""}
             </TableBody>
           </Table>
 
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'})
-});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+        'id' : 1,
+        'image' : 'https://placeimg.com/64/64/any',
+        'name' : '홍길동',
+        'birthday' : '990419',
+        'gender' : '남성',
+        'job' : '대학생'  
+       },
+       {
+        'id' : 2,
+        'image' : 'https://placeimg.com/64/64/1',
+        'name' : '김신',
+        'birthday' : '970709',
+        'gender' : '남성',
+        'job' : '프로그래머'  
+       },
+       {
+        'id' : 3,
+        'image' : 'https://placeimg.com/64/64/2',
+        'name' : '김영희',
+        'birthday' : '920719',
+        'gender' : '여자',
+        'job' : '직장인'  
+       }
+      ])
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
